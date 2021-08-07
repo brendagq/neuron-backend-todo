f5a0f7fda6da0a9d1e7512603c06e9bba039c927
brendagq <brennnda.gonzalezq@gmail.com>
2021-08-07 16:13:46 -0600

    Port

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ require('dotenv').config()
 const dbConnect = require('./src/lib/db')
 const server = require('./src/server')
 
+const port = process.env.PORT || 3000
 const listenServer = function () {
   return new Promise((resolve, reject) => {
-    server.listen(8080, () => {
+    server.listen( port , () => {
         console.log('Server Up')
         resolve()
     })
